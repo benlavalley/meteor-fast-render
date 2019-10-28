a595a2b401ca5a08bdd505821854313c9a8684e1
Ben Lavalley <ben.lavalley@kaseya.com>
2019-10-28 01:26:15 -0400

    version bump

--- package.js
+++ package.js
@@ -16,7 +16,7 @@ Package.onUse(function(api) {
 	api.versionsFrom('METEOR@1.8.1')
 	api.mainModule('lib/client/fast_render.js', 'client')
 	api.mainModule('lib/server/namespace.js', 'server')
-	api.use('staringatlights:inject-data@2.3.1', ['client', 'server'])
+	api.use('staringatlights:inject-data@2.3.2', ['client', 'server'])
 	api.use('chuangbo:cookie@1.1.1', 'client')
 	api.use('meteorhacks:picker@1.0.5', 'server')
 	api.use('lamhieu:meteorx@2.1.2', 'server')
